3716d1341cd46df96ad0f912a650a914bcc224c8
NATH-RIMON <rimonnath11@gmail.com>
2021-09-19 12:56:21 +0600

    first cv

---
+++ CV/CV/Controllers/ProfileController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CV.Controllers
+{
+    public class ProfileController : Controller
+    {
+        // GET: Profile
+        public ActionResult Bio()
+        {
+            ViewBag.Name = "RIMON NATH";
+
+            return View();
+            
+        }
+
+        public ActionResult Education()
+        {
+            return View();
+        }
+        public ActionResult Project()
+        {
+          
+            return View();
+        }
+
+        public ActionResult Reference()
+        {
+            ViewBag.Rname = "Tanvir Ahmed";
+            ViewBag.Details = "Lecturer,Department of Computer Science, AIUB ";
+            return View();
+        }
+    }
+}
