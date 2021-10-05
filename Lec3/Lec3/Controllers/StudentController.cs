f22d9e411434208820f4d1e2697e74bca6cc016b
NATH-RIMON <rimonnath11@gmail.com>
2021-10-06 00:25:31 +0600

    full

---
+++ Lec3/Lec3/Controllers/StudentController.cs
@@ -0,0 +1,32 @@
+﻿using Lec3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lec3.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Student s)
+        {
+            if(ModelState.IsValid)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+}
---
+++ Lec3/Lec3/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Lec3.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string DOB { get; set; }
+        public string Gender { get; set; }
+
+        public Double Cgpa { get; set; }
+    }
+}
